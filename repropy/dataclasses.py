73494254ac7e4ebaff147e1df297702e6b7f4ac5
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-03-25 23:49:28 +0300

    Add wrapper class for values that doesn't use meta

--- repropy/dataclasses.py
+++ repropy/dataclasses.py
@@ -2,7 +2,7 @@
 """
 """
 from dataclasses import is_dataclass, fields
-
+import hashlib
 
 def instantiate(cls, dct):
     """
@@ -21,3 +21,26 @@ def instantiate(cls, dct):
         else:
             par[nm] = dct[nm]
     return cls(**par)
+
+
+def no_meta(fun):
+    def wrapper(meta):
+        return NoMeta(fun)
+    return wrapper
+
+class NoMeta(object):
+    """
+    Wrapper class for functions which does not make use of metadata
+    """
+    def __init__(self, fun):
+        acc = hashlib.sha256()
+        acc.update( fun.__module__.encode('utf-8') )
+        acc.update( fun.__name__.encode('utf-8') )
+        self.h   = acc.digest()
+        self.fun = fun
+
+    def __call__(self, *args, **kwargs):
+        return self.fun(*args, **kwargs)
+
+    def hash(self):
+        return self.h
--- repropy/store.py
+++ repropy/store.py
@@ -7,7 +7,7 @@ import pickle
 
 class Store(object):
     """
-    Hash-adressable storage for python object    
+    Hash-adressable storage for python object
     """
     def __init__(self,path):
         self.path = path
@@ -24,15 +24,15 @@ class Store(object):
                 return (pickle.load(h), True)
         except FileNotFoundError:
             return (None, False)
-                
+
     def store(self, h, val):
         """
-        Store value 
+        Store value
         """
         nm = self._path(h)
         with open(nm,'wb') as h:
             pickle.dump(val, h)
 
     def _path(self, h):
-        s  = base64.urlsafe_b64encode(h).decode('utf-8')
+        s  = base64.urlsafe_b64encode(h).decode('utf-8').replace('=', '')
         return os.path.join(self.path, s)
