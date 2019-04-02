1a9992ab1599d34135078e5e0d9bafed6f018314
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-04-02 20:22:11 +0300

    Working PyMC3
    Version from 18.09 did not wwork at all. Some sort of memory corrupion
    or whetnot

--- shell.nix
+++ shell.nix
@@ -1,39 +1,26 @@
 # Nix expression for starting jupyter notebook
 let
-  pkgs    = import <nixpkgs> {config = {}; overlays=[overlay];};
+  pkgs   = import (builtins.fetchGit {
+    name = "19.03";
+    url  = https://github.com/nixos/nixpkgs/;
+    ref  = "7e889fe8c81c45faf8c7964cd89eafb1f2cd6ec4";
+  }) {
+    config = {};
+    overlays=[overlay];
+  };
+  #
   overlay = self: super: {
-    # Still broken
-    clblas = self.lib.overrideDerivation super.clblas (oldAttrs: {
-      cmakeFlags = [];
-    });
     python37 = super.python37.override { packageOverrides = pyOverrides; };
   };
   # Python overrides
   pyOverrides = self: super: {
-    libgpuarray = super.libgpuarray.override {
-      openclSupport = false;
-    };
-    Theano = self.callPackage ./nix/Theano.nix rec {
-      cudaSupport = self.config.cudaSupport or false;
-      cudnnSupport = cudaSupport;
-      inherit (pkgs.linuxPackages) nvidia_x11;
-    };
-    # Disable tests in some packages
-    #
-    cython = super.cython.overridePythonAttrs(old: rec {
-      checkPhase = "";
-    });
-    #
-    hypothesis = super.hypothesis.overridePythonAttrs(old: rec {
-      checkPhase = "";
-    });
-    #
-    sure = super.sure.overridePythonAttrs(old: rec {
-      checkPhase = "";
-    });
-    #
-    patsy = super.patsy.overridePythonAttrs(old: rec {
-      checkPhase = "";
+    joblib = super.joblib.overridePythonAttrs(old: rec {
+      version = "0.12.5";
+      src = super.fetchPypi {
+        inherit version;
+        pname  = "joblib";
+        sha256 = "11cdfd38cdb71768149e1373f2509e9b4fc1ec6bc92f874cb515b25f2d69f8f4";
+      };
     });
   };
   #
@@ -55,9 +42,14 @@ in
 #pkgs.mkShell {buildInputs = [pkgs.clblas]; }
 pkgs.mkShell {
   buildInputs = [ pyp ];
+  # TMP&Co overrides needed for Theano https://github.com/NixOS/nixpkgs/issues/4546
   shellHook   = ''
     export PYTHONPATH=$PWD:$PWD/unfolding
     export XDG_DATA_HOME=$PWD/.XDG
     export JUPYTER_CONFIG_DIR=$PWD/.XDG/jupyter
+    export TMP=/tmp
+    export TMPDIR=/tmp
+    export TEMPDIR=/tmp
+    export TEMP=/tmp
     '';
 }
